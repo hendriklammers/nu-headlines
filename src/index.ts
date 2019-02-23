4f0e7a7b6a49064c13a910dae11fbf8fc432165a
Hendrik Lammers <lammers@gmail.com>
2019-02-23 21:03:22 +0700

    Display url of selected headline

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,51 @@
-import Parser from 'rss-parser'
+import Parser, { Items } from 'rss-parser'
 import ora from 'ora'
 import inquirer from 'inquirer'
 
-const parser = new Parser()
-const spinner = ora({
-  color: 'white',
-  text: 'Loading rss feed',
-}).start()
+interface FeedItem {
+  title: string
+  link: string
+}
+
+interface Choice {
+  name: string
+  value: string
+}
+
+const feedToChoices = (feed: Items): Promise<Choice[]> => {
+  return new Promise((resolve, reject) => {
+    if (feed.items) {
+      const choices = feed.items.map((item: FeedItem, index: number) => {
+        return {
+          name: item.title,
+          value: item.link,
+        }
+      })
+      resolve(choices)
+    } else {
+      reject('No feed items available')
+    }
+  })
+}
 
 const main = async () => {
+  const spinner = ora({
+    color: 'white',
+    text: 'Loading rss feed',
+  }).start()
+  const parser = new Parser()
   const feed = await parser.parseURL('https://www.nu.nl/rss/Algemeen')
   spinner.stop()
-
-  if (feed.items) {
-    const choices = feed.items.map((item, index) => {
-      return {
-        name: item.title as string,
-        value: item.link as string,
-        short: item.content as string,
-      }
-    })
-
-    inquirer.prompt([
-      {
-        type: 'rawlist',
-        name: 'headlines',
-        message: 'Select headline',
-        choices,
-        pageSize: 100,
-      },
-    ])
-
-    // console.log(headlines.join('\n') + '\n')
-  }
+  const choices = await feedToChoices(feed)
+  const selected = await inquirer.prompt([
+    {
+      type: 'rawlist',
+      name: 'headlines',
+      message: 'Select headline',
+      choices,
+      pageSize: 100,
+    },
+  ])
 }
 
 main().catch(console.error)
